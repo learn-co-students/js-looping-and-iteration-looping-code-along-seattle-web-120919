2d26a0dfd4aad357eb33a77b5b3a7e6f26b0ce63
christinamcmahon <christina.mcmahon1@gmail.com>
2020-01-27 15:42:46 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+
+function writeCards(names) {
+    const result = [];
+    for (let i = 0; i < names.length; i++) {
+        result.push("Thank you, " + names[i] + ", for the wonderful surprise gift!");
+    };
+    return result;
+}
+
+function countDown(max) {
+    for (let i = max; i >= 0; i--) {
+        console.log(i);
+    }
+}
